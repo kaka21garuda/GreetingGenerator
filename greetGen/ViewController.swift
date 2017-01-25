70b629bccb22bd527ed8112ebd975cbdbb967245
Buka Cakrawala <kaka10garuda@gmail.com>
2017-01-25 12:16:10 -0800

    added UI

--- greetGen/ViewController.swift
+++ greetGen/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
+enum MainSegmentState: Int {
+    case useButton
+    case useTextfield
+}
+
+enum TextFieldTag: Int {
+    case greetingTextField
+    case nameTextField
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var mainSegment: UISegmentedControl!
+    
+    @IBOutlet weak var customGreetingTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet var greetingsButton: [UIButton]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
